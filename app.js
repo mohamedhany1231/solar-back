11aaf3eda3d492dc55f0e6c3d6f67da5216feeea
mohamedhany1231 <mohamedhany61@gmail.com>
2024-06-20 18:03:58 +0300

    fix rate limit

--- app.js
+++ app.js
@@ -28,7 +28,8 @@ const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
   message: "Too many requests from this IP, please try again in an hour!",
-  Proxy: true,
+  proxy: true,
+  keyGenerator: (req) => req.ip,
 });
 app.use("/api", limiter);
 
