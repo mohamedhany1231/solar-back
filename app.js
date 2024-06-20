4e5e18fa89626c51746d54e0c488e6ee2babe1d1
mohamedhany1231 <mohamedhany61@gmail.com>
2024-06-20 18:01:31 +0300

    edit cookies

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ const ReadingsRouter = require("./routes/ReadingsRouter");
 const helmet = require("helmet");
 
 const app = express();
-app.enable("trust proxy");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: "10kb" }));
@@ -29,6 +28,7 @@ const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
   message: "Too many requests from this IP, please try again in an hour!",
+  Proxy: true,
 });
 app.use("/api", limiter);
 
